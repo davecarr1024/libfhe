aae1fa7ef8fa3f5e2594983b107999d2d799d94d
dave.carr.re2 <dave.carr.re2@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2009-10-15 15:55:31 +0000

    update fhe3

--- trunk/fhe3/fhe/Entity.cpp
+++ trunk/fhe3/fhe/Entity.cpp
@@ -1,6 +1,10 @@
 #include "Entity.h"
 #include "AspectFactory.h"
+#include "FileSystem.h"
+
 #include <sstream>
+#include <cstdarg>
+#include <cstdio>
 
 namespace fhe
 {
@@ -31,6 +35,28 @@ namespace fhe
     
     Entity::~Entity()
     {
+    }
+    
+    boost::python::object Entity::defineClass()
+    {
+        return boost::python::class_<Entity,boost::noncopyable>("Entity",boost::python::no_init)
+            .def("__getattr__",&Entity::getAttr)
+            .def("__setattr__",&Entity::setAttr)
+            .def("hasVar",&Entity::pyHasVar)
+            .def("getVar",&Entity::pyGetVar)
+            .def("getVar",&Entity::pyGetVarDef)
+            .def("setVar",&Entity::pySetVar)
+            .def("publish",&Entity::pyPublish)
+            .def("loadChild",&Entity::pyLoadChild)
+            .def("buildChild",&Entity::pyBuildChild)
+            .def("hasChild",&Entity::hasChild)
+            .def("getChild",&Entity::pyGetChild)
+            .def("getParent",&Entity::pyGetParent)
+            .def("hasFunc",&Entity::pyHasFunc)
+            .def("getRoot",&Entity::pyGetRoot)
+            .add_property("name",&Entity::getName)
+            .add_property("path",&Entity::getPath)
+        ;
     }
     
     std::string Entity::getName()
@@ -72,6 +98,7 @@ namespace fhe
             if ( m_parent )
             {
                 m_parent->addChild(EntityPtr(this,true));
+                callAll<void>("on_attach");
             }
         }
     }
@@ -84,6 +111,7 @@ namespace fhe
             m_parent = 0;
             updatePath();
             parent->removeChild(EntityPtr(this,true));
+            callAll<void>("on_detach");
         }
     }
     
@@ -121,6 +149,18 @@ namespace fhe
         return EntityPtr(m_parent,true);
     }
     
+    boost::python::object Entity::pyGetChild( const std::string& name )
+    {
+        EntityPtr child( getChild(name) );
+        return child ? child->toPy() : boost::python::object();
+    }
+    
+    boost::python::object Entity::pyGetParent()
+    {
+        EntityPtr parent = getParent();
+        return parent ? parent->toPy() : boost::python::object();
+    }
+    
     AspectPtr Entity::addAspect( const std::string& name )
     {
         if ( !hasAspect(name) )
@@ -129,6 +169,10 @@ namespace fhe
             assert(aspect);
             m_aspects[aspect->getName()] = aspect;
             aspect->setEntity(EntityPtr(this,true));
+            if ( aspect->hasFunc<void,void>("on_attach") )
+            {
+                aspect->call<void>("on_attach");
+            }
             return aspect;
         }
         else
@@ -144,6 +188,10 @@ namespace fhe
             AspectPtr aspect = m_aspects[name];
             m_aspects.erase(name);
             aspect->setEntity(0);
+            if ( aspect->hasFunc<void,void>("on_detach") )
+            {
+                aspect->call<void>("on_detach");
+            }
         }
     }
     
@@ -161,56 +209,28 @@ namespace fhe
     {
         Var var;
         std::string getName = "get_" + name;
-        for ( AspectMap::iterator i = m_aspects.begin(); i != m_aspects.end(); ++i )
+        if ( hasFunc<Var,void>(getName) )
         {
-            if ( i->second->hasFunc<Var,void>(getName) )
-            {
-                var = i->second->call<Var>(getName);
-                if ( !var.empty() )
-                {
-                    return var;
-                }
-            }
+            var = call<Var>(getName);
         }
         return var;
     }
     
     void Entity::onSetVar( const std::string& name, const Var& var )
     {
-        std::string setName = "set_" + name;
-        for ( AspectMap::iterator i = m_aspects.begin(); i != m_aspects.end(); ++i )
-        {
-            if ( i->second->hasFunc<void,Var>(setName) )
-            {
-                i->second->call<void,Var>(setName,var);
-            }
-        }
+        callAll<void,Var>("set_" + name,var);
     }
     
     void Entity::publish( const std::string& cmd, const VarMap& args )
     {
-        std::string msgName = "msg_" + cmd;
-        for ( AspectMap::iterator i = m_aspects.begin(); i != m_aspects.end(); ++i )
-        {
-            if ( i->second->hasFunc<void,VarMap>(msgName) )
-            {
-                i->second->call<void,VarMap>(msgName,args);
-            }
-        }
+        callAll<void,VarMap>("msg_" + cmd,args);
         
         for ( EntityMap::iterator i = m_children.begin(); i != m_children.end(); ++i )
         {
             i->second->publish(cmd,args);
         }
 
-        std::string unmsgName = "unmsg_" + cmd;
-        for ( AspectMap::iterator i = m_aspects.begin(); i != m_aspects.end(); ++i )
-        {
-            if ( i->second->hasFunc<void,VarMap>(unmsgName) )
-            {
-                i->second->call<void,VarMap>(unmsgName,args);
-            }
-        }
+        callAll<void,VarMap>("unmsg_" + cmd,args);
     }
     
     void Entity::pyPublish( const std::string& cmd, boost::python::dict args )
@@ -232,7 +252,7 @@ namespace fhe
                 return boost::python::object(i->second->pyGetFunc(name));
             }
         }
-        throw std::runtime_error( "name error: " + name );
+        error("name error: %s", name.c_str());
     }
     
     void Entity::setAttr( const std::string& name, boost::python::object val )
@@ -240,19 +260,152 @@ namespace fhe
         pySetVar(name,val);
     }
     
-    boost::python::object Entity::defineClass()
+    EntityPtr Entity::loadChild( const std::string& filename )
     {
-        return boost::python::class_<Entity,boost::noncopyable>("Entity",boost::python::no_init)
-            .def("__getattr__",&Entity::getAttr)
-            .def("__setattr__",&Entity::setAttr)
-            .def("hasVar",&Entity::pyHasVar)
-            .def("getVar",&Entity::pyGetVar)
-            .def("getVar",&Entity::pyGetVarDef)
-            .def("setVar",&Entity::pySetVar)
-            .def("publish",&Entity::pyPublish)
-            .add_property("name",&Entity::getName)
-            .add_property("path",&Entity::getPath)
-        ;
+        TiXmlDocument doc;
+        if ( doc.LoadFile( FileSystem::instance().getFile(filename).c_str() ) )
+        {
+            return loadChildData( &doc );
+        }
+        else
+        {
+            error("unable to load file %s", filename.c_str());
+        }
     }
     
+    boost::python::object Entity::pyLoadChild( const std::string& filename )
+    {
+        return loadChild(filename)->toPy();
+    }
+    
+    EntityPtr Entity::loadChildData( TiXmlHandle h )
+    {
+        std::string name = "Ent";
+        for ( TiXmlElement* e = h.FirstChildElement().ToElement(); e; e = e->NextSiblingElement() )
+        {
+            if ( !strcmp(e->Value(),"name") )
+            {
+                name = e->GetText();
+            }
+        }
+        
+        EntityPtr child( new Entity(name) );
+        
+        addChild(child);
+        child->load(h);
+        return child;
+    }
+    
+    void Entity::load( TiXmlHandle h )
+    {
+        loadTag(h,"vars");
+        loadTag(h,"aspects");
+        loadTag(h,"vars");
+        loadTag(h,"children");
+    }
+    
+    void Entity::loadTag( TiXmlHandle h, const std::string& tag )
+    {
+        printf("%s load %s\n",getPath().c_str(),tag.c_str());
+        for ( TiXmlElement* e = h.FirstChildElement().ToElement(); e; e = e->NextSiblingElement() )
+        {
+            if ( e->Value() == tag )
+            {
+                if ( tag == "vars" )
+                {
+                    loadVars(e);
+                }
+                else if ( tag == "aspects" )
+                {
+                    loadAspects(e);
+                }
+                else if ( tag == "children" )
+                {
+                    loadChildren(e);
+                }
+            }
+        }
+    }
+    
+    void Entity::loadVars( TiXmlHandle h )
+    {
+        for ( TiXmlElement* e = h.FirstChildElement("var").ToElement(); e; e = e->NextSiblingElement("var") )
+        {
+            const char *name = e->Attribute("name");
+            assert(name);
+            boost::python::dict ns = Aspect::emptyNamespace();
+            ns["entity"] = toPy();
+            boost::python::object val = Aspect::tryEvalScript(e->GetText(),ns);
+            pySetVar(name,val);
+        }
+    }
+    
+    void Entity::loadChildren( TiXmlHandle h )
+    {
+        for ( TiXmlElement* e = h.FirstChildElement("child").ToElement(); e; e = e->NextSiblingElement("child") )
+        {
+            loadChildData(e);
+        }
+    }
+    
+    void Entity::loadAspects( TiXmlHandle h )
+    {
+        for ( TiXmlElement* e = h.FirstChildElement("aspect").ToElement(); e; e = e->NextSiblingElement("aspect") )
+        {
+            const char* name = e->Attribute("name");
+            assert(name);
+            AspectPtr aspect( addAspect(name) );
+            assert(aspect);
+            aspect->load(e);
+        }
+    }
+    
+    EntityPtr Entity::buildChild( const std::string& name )
+    {
+        EntityPtr child( new Entity(name) );
+        addChild(child);
+        return child;
+    }
+    
+    boost::python::object Entity::pyBuildChild( const std::string& name )
+    {
+        return buildChild(name)->toPy();
+    }
+    
+    boost::python::object Entity::toPy()
+    {
+        return boost::python::object(boost::python::ptr(this));
+    }
+
+    void Entity::error( const char* format, ... )
+    {
+        char buffer[1024];
+        va_list ap;
+        va_start(ap,format);
+        vsnprintf( buffer, 1024, format, ap );
+        va_end(ap);
+        throw std::runtime_error( m_path + ": ERROR: " + buffer );
+    }
+    
+    bool Entity::pyHasFunc( const std::string& name )
+    {
+        for ( AspectMap::iterator i = m_aspects.begin(); i != m_aspects.end(); ++i )
+        {
+            if ( i->second->hasFuncName(name) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    EntityPtr Entity::getRoot()
+    {
+        return m_parent ? m_parent->getRoot() : EntityPtr(this,true);
+    }
+    
+    boost::python::object Entity::pyGetRoot()
+    {
+        return getRoot()->toPy();
+    }
 }
