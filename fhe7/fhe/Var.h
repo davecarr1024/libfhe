c7ddd0e7ed507454ade67a5e7761644aab06b910
dave.carr.re2 <dave.carr.re2@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2010-12-09 16:25:40 +0000

    var serializers

--- fhe7/fhe/Var.h
+++ fhe7/fhe/Var.h
@@ -9,8 +9,6 @@
 namespace fhe
 {
     
-    void deserialize( const YAML::Node& node, int& i );
-    
     class IVar
     {
         public:
@@ -18,55 +16,67 @@ namespace fhe
             virtual void set( const Val& v ) = 0;
             virtual bool trySet( const Val& v ) = 0;
             virtual std::string name() const = 0;
-            virtual void deser( const YAML::Node& node ) = 0;
-            virtual bool canSer() const = 0;
-            virtual void ser( YAML::Emitter& out ) const = 0;
+            virtual void deserialize( const YAML::Node& node ) = 0;
+            virtual bool canSerialize() const = 0;
+            virtual void serialize( YAML::Emitter& out ) const = 0;
     };
     
     typedef boost::shared_ptr<IVar> IVarPtr;
     
-    template <bool>
-    struct deserialize_impl;
-    
-    template<>
-    struct deserialize_impl<true>
-    {
-        template <class T>
-        static void deser( const YAML::Node& node, T& t )
-        {
-            deserialize( node, t );
-        }
-    };
-    
-    template<>
-    struct deserialize_impl<false>
-    {
-        template <class T>
-        static void deser( const YAML::Node& node, T& t )
-        {
-            FHE_ERROR( "trying to deserialize unknown type %s", typeid(T).name() );
-        }
-    };
-    
-    namespace deserialize_fallback
-    {
-        struct flag { char c[2]; };
-        flag deserialize( ... );
-        
-        int operator,( flag, flag );
-        
-        template <typename T>
-        char operator,( flag, T const& );
-        
-        char operator,( int, flag );
-        int operator,( char, flag );
-    }
-    
-    template <class TObj, class TVar>
-    class Var : public IVar
+    template <class T>
+    class TVar : public IVar
     {
         public:
-            typedef TVar (TObj::*Ptr );
+            virtual T& var()=0;
+            virtual const T& var() const = 0;
+            
+            void deserialize( const YAML::Node& node )
+            {
+                FHE_ERROR( "trying to deserialize %s", typeid(T).name() );
+            }
+            
+            bool canSerialize() const
+            {
+                return false;
+            }
+            
+            void serialize( YAML::Emitter& out ) const
+            {
+                FHE_ERROR( "trying to serialize %s", typeid(T).name() );
+            }
+    };
+    
+    #define TVAR_impl( type ) \
+        template <> \
+        class TVar<type> : public IVar \
+        { \
+            public: \
+                virtual type& var()=0; \
+                virtual const type& var() const = 0; \
+                void deserialize( const YAML::Node& node ) \
+                { \
+                    node >> var(); \
+                } \
+                bool canSerialize() const \
+                { \
+                    return true; \
+                } \
+                void serialize( YAML::Emitter& out ) const \
+                { \
+                    out << var(); \
+                } \
+        };
+        
+    TVAR_impl( bool );
+    TVAR_impl( int );
+    TVAR_impl( double );
+    TVAR_impl( std::string );
+    
+    template <class TObj, class T>
+    class Var : public TVar<T>
+    {
+        public:
+            typedef T (TObj::*Ptr );
             
         private:
             std::string m_name;
@@ -81,6 +91,16 @@ namespace fhe
             {
             }
             
+            T& var()
+            {
+                return m_obj->*m_ptr;
+            }
+            
+            const T& var() const
+            {
+                return m_obj->*m_ptr;
+            }
+            
             Val get() const
             {
                 return Val( m_obj->*m_ptr );
@@ -88,16 +108,16 @@ namespace fhe
             
             void set( const Val& v )
             {
-                FHE_ASSERT_MSG( v.is<TVar>(), "var type mismatch: expected %s got %s", 
-                                typeid(TVar).name(), v.type().c_str() );
-                m_obj->*m_ptr = v.get<TVar>();
+                FHE_ASSERT_MSG( v.is<T>(), "var type mismatch: expected %s got %s", 
+                                typeid(T).name(), v.type().c_str() );
+                m_obj->*m_ptr = v.get<T>();
             }
             
             bool trySet( const Val& v )
             {
-                if ( v.is<TVar>() )
+                if ( v.is<T>() )
                 {
-                    m_obj->*m_ptr = v.get<TVar>();
+                    m_obj->*m_ptr = v.get<T>();
                     return true;
                 }
                 else
@@ -109,24 +129,6 @@ namespace fhe
             std::string name() const
             {
                 return m_name;
-            }
-            
-            void deser( const YAML::Node& node )
-            {
-                using namespace deserialize_fallback;
-                TVar& t = m_obj->*m_ptr;
-                deserialize_impl
-                    <sizeof( deserialize_fallback::flag(), deserialize( node, t ), deserialize_fallback::flag() ) != 1>
-                    ::deser( node, t );
-            }
-            
-            bool canSer() const
-            {
-                return false;
-            }
-            
-            void ser( YAML::Emitter& out ) const
-            {
             }
     };
     
