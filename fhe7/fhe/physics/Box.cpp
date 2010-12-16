d974017ee3ba22eef77e00a559ba0b7b5baacc92
dave.carr.re2 <dave.carr.re2@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2010-12-16 16:53:11 +0000

    reorganize to make everything a module

---
+++ fhe7/fhe/physics/Box.cpp
@@ -0,0 +1,19 @@
+// #include <physics/Box.h>
+// #include <physics/World.h>
+// 
+// namespace fhe
+// {
+//     namespace physics
+//     {
+//         
+//         FHE_NODE( Box );
+//         FHE_DEP( Box, Body );
+// //         FHE_VAR( Box, size );
+//         
+//         btCollisionShape* Box::makeShape()
+//         {
+// //             return new btBoxShape( World::convert( size ) );
+//         }
+//         
+//     }
+// }
