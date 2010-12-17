76e944d441962f196d7262663c1e8dfce45b4de1
dave.carr.re2 <dave.carr.re2@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2010-12-17 15:50:14 +0000

    rename physics to physics3

--- fhe7/tests/physics3_test.cpp
+++ fhe7/tests/physics3_test.cpp
@@ -1,7 +1,7 @@
 #include <fhe/sim/Sim.h>
 #include <fhe/sim/IUpdate.h>
-#include <fhe/physics/World.h>
-#include <fhe/physics/Box.h>
+#include <fhe/physics3/World.h>
+#include <fhe/physics3/Box.h>
 #include <gtest/gtest.h>
 using namespace fhe;
 
@@ -15,7 +15,7 @@ class TestNode : public Node, public sim::IUpdate
                 NodePtr parent = this->parent();
                 ASSERT_TRUE( parent );
                 Vec3 pos;
-                ASSERT_TRUE( parent->tryCall( &physics::Body::getPosition, pos ) );
+                ASSERT_TRUE( parent->tryCall( &physics3::Body::getPosition, pos ) );
                 ASSERT_LT( Math::abs( pos.z ), 1 );
                 ancestorCall( &sim::Sim::shutdown );
             }
@@ -26,15 +26,15 @@ FHE_NODE( TestNode );
 FHE_DEP( TestNode, sim, IUpdate );
 FHE_FUNC( TestNode, update );
 
-TEST( physics_test, falling_box )
+TEST( physics3_test, falling_box )
 {
     NodePtr sim( new sim::Sim );
     
-    NodePtr world( new physics::World );
+    NodePtr world( new physics3::World );
     sim->attachChild( world );
     
-    NodePtr box( new physics::Box );
-    box->call( &physics::Body::setPosition, Vec3( 0, 0, 20 ) );
+    NodePtr box( new physics3::Box );
+    box->call( &physics3::Body::setPosition, Vec3( 0, 0, 20 ) );
     world->attachChild( box );
     
     NodePtr test( new TestNode );
