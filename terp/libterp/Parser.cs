6e7a241189ff75baf4cb00da140b5dbe6a699f27
dave.carr.re2@gmail.com <dave.carr.re2@gmail.com@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2012-09-17 21:34:31 +0000

    initial terp

---
+++ terp/libterp/Parser.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+namespace libterp
+{
+  public class Parser
+  {
+    public class Result
+    {
+      public string Type { get; private set; }
+
+      public string Value { get; private set; }
+
+      public List<Result> Children { get; private set; }
+
+      public Result(string type, string value)
+      {
+        Type = type;
+        Value = value;
+        Children = new List<Result>();
+      }
+
+      public Result(string type, params Result[] children)
+      {
+        Type = type;
+        Children = new List<Result>(children);
+      }
+    };
+
+    internal class Context
+    {
+      private List<Lexer.Token> tokens;
+
+      private int position = 0;
+
+      private Stack<int> PositionStack = new Stack<int>();
+
+      public Context(List<Lexer.Token> tokens)
+      {
+        this.tokens = tokens;
+      }
+
+      public Lexer.Token Next()
+      {
+        if (position < tokens.Count)
+        {
+          return tokens[position++];
+        }
+        else
+        {
+          return null;
+        }
+      }
+
+      public void Push()
+      {
+        PositionStack.Push(position);
+      }
+
+      public void Pop()
+      {
+        position = PositionStack.Pop();
+      }
+    }
+
+    public class Rule
+    {
+      public enum Type
+      {
+        Token,
+        Or,
+        And,
+        OneOrMore,
+      };
+
+      public string Name { get; private set; }
+
+      public Type type { get; private set; }
+
+      public List<Rule> Children { get; private set; }
+
+      public Rule(Type type, string name, params Rule[] children)
+      {
+        this.type = type;
+        Name = name;
+        Children = new List<Rule>(children);
+      }
+
+      internal Result Parse(Context context)
+      {
+        switch (type)
+        {
+          case Type.Token:
+            {
+              context.Push();
+              Lexer.Token token = context.Next();
+              if (token != null && token.Type == Name)
+              {
+                return new Result(Name, token.Value);
+              }
+              context.Pop();
+              return null;
+            }
+          case Type.Or:
+            {
+              foreach (Rule rule in Children)
+              {
+                context.Push();
+                Result result = rule.Parse(context);
+                if (result != null)
+                {
+                  return result;
+                }
+                context.Pop();
+              }
+              return null;
+            }
+          case Type.And:
+            {
+              context.Push();
+              Result result = new Result(Name);
+              foreach (Rule rule in Children)
+              {
+                Result childResult = rule.Parse(context);
+                if (childResult == null)
+                {
+                  context.Pop();
+                  return null;
+                }
+                else
+                {
+                  result.Children.Add(childResult);
+                }
+              }
+              return result;
+            }
+          case Type.OneOrMore:
+            {
+              context.Push();
+              Result result = new Result(Name);
+              Result childResult = Children.First().Parse(context);
+              if (childResult == null)
+              {
+                context.Pop();
+                return null;
+              }
+              else
+              {
+                while (childResult != null)
+                {
+                  context.Push();
+                  result.Children.Add(childResult);
+                  childResult = Children.First().Parse(context);
+                  if (childResult == null)
+                  {
+                    context.Pop();
+                  }
+                }
+                return result;
+              }
+            }
+          default:
+            return null;
+        }
+      }
+    }
+
+    public Rule Root { get; set; }
+
+    public Lexer Lexer { get; set; }
+
+    public Result Parse(string s)
+    {
+      return Root.Parse(new Context(Lexer.Lex(s)));
+    }
+
+    public Parser(Rule root, Lexer lexer)
+    {
+      Root = root;
+      Lexer = lexer;
+    }
+
+    public Parser(string s)
+    {
+      Lexer lexer = new Lexer(
+        new Lexer.Rule("enddecl", @";"),
+        new Lexer.Rule("ruleop", @"=>"),
+        new Lexer.Rule("tokenop", @"="),
+        new Lexer.Rule("orop", @"\|"),
+        new Lexer.Rule("andop", @"\+"),
+        new Lexer.Rule("lparen", @"\("),
+        new Lexer.Rule("rparen", @"\)"),
+        new Lexer.Rule("oneormoreop", @"\*"),
+        new Lexer.Rule("delimop", @"\(delim\)"),
+        new Lexer.Rule("id", @"\w+"),
+        new Lexer.Rule("regex", @"\S+"),
+        new Lexer.Rule("ws", @"\s+", true)
+        );
+
+      Parser parser = new Parser(
+        new Rule(Rule.Type.OneOrMore, "grammar",
+          new Rule(Rule.Type.And, "decl",
+            new Rule(Rule.Type.Or, "decl2",
+              new Rule(Rule.Type.And, "rule",
+                new Rule(Rule.Type.Token, "id"),
+                new Rule(Rule.Type.Token, "ruleop"),
+                new Rule(Rule.Type.Token, "id")
+              ),
+              new Rule(Rule.Type.And, "token",
+                new Rule(Rule.Type.Token, "id"),
+                new Rule(Rule.Type.Token, "tokenop"),
+                new Rule(Rule.Type.Token, "id")
+              )
+            ),
+            new Rule(Rule.Type.Token, "enddecl")
+          )
+        ),
+        lexer
+      );
+
+      Result result = parser.Parse(s);
+    }
+
+  }
+}
---
+++ terp/terptest/TerpTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using libterp;
+
+namespace terptest
+{
+  /// <summary>
+  /// Summary description for TerpTest
+  /// </summary>
+  [TestClass]
+  public class TerpTest
+  {
+    public TerpTest()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    //[TestMethod]
+    public void LexTest()
+    {
+      Lexer lexer = new Lexer(
+        new Lexer.Rule("whitespace", @"\s+"),
+        new Lexer.Rule("nonwhitespace", @"\w+")
+        );
+
+      List<Lexer.Token> tokens = lexer.Lex(" abc\t");
+      Assert.AreEqual(3, tokens.Count);
+      Assert.AreEqual("whitespace", tokens[0].Type);
+      Assert.AreEqual(" ", tokens[0].Value);
+      Assert.AreEqual("nonwhitespace", tokens[1].Type);
+      Assert.AreEqual("abc", tokens[1].Value);
+      Assert.AreEqual("whitespace", tokens[2].Type);
+      Assert.AreEqual("\t", tokens[2].Value);
+    }
+
+    //[TestMethod]
+    public void ParseTest()
+    {
+      Lexer lexer = new Lexer(
+        new Lexer.Rule("whitespace", @"\s+", true),
+        new Lexer.Rule("leftParen", @"\("),
+        new Lexer.Rule("rightParen", @"\)"),
+        new Lexer.Rule("id", @"[\D\w]\w*")
+        );
+
+
+      Parser parser = new Parser(
+        new Parser.Rule(Parser.Rule.Type.And,"expression",
+          new Parser.Rule(Parser.Rule.Type.Token,"leftParen"),
+          new Parser.Rule(Parser.Rule.Type.OneOrMore,"expressionBody",
+            new Parser.Rule(Parser.Rule.Type.Token,"id")
+            ),
+          new Parser.Rule(Parser.Rule.Type.Token,"rightParen")
+        ),
+        lexer
+      );
+
+      Parser.Result result = parser.Parse("( a )");
+      Assert.IsNotNull(result);
+      Assert.AreEqual("expression", result.Type);
+      Assert.AreEqual(3, result.Children.Count);
+      Assert.AreEqual("leftParen", result.Children[0].Type);
+      Assert.AreEqual("expressionBody", result.Children[1].Type);
+      Assert.AreEqual(1, result.Children[1].Children.Count);
+      Assert.AreEqual("id", result.Children[1].Children[0].Type);
+      Assert.AreEqual("a", result.Children[1].Children[0].Value);
+      Assert.AreEqual("rightParen", result.Children[2].Type);
+    }
+
+    [TestMethod]
+    public void ParseParseTest()
+    {
+      Parser parser = new Parser(@"
+        a => b ;
+      ");
+
+      //Parser.Result result = parser.Parse("(a)");
+      //Assert.IsNotNull(result);
+    }
+  }
+}
