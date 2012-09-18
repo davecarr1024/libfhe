5bebc356bd33354ea3bb2a75efbf57901f75fe04
dave.carr.re2@gmail.com <dave.carr.re2@gmail.com@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2012-09-18 03:31:33 +0000

    dfs

---
+++ trunk/terp/interp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using libterp;
+
+namespace interp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lexer lexer = new Lexer(
+                new Lexer.Rule("lparen", @"\("),
+                new Lexer.Rule("rparen", @"\)"),
+                new Lexer.Rule("id", @"[a-zA-Z]\w*"),
+                new Lexer.Rule("ws", @"\s+", true)
+                );
+            List<Lexer.Result> tokens = lexer.Lex("(a)");
+            System.Diagnostics.Debug.Assert(
+                tokens.Count == 3 && 
+                tokens[0].Type == "lparen" && 
+                tokens[0].Value == "(" &&
+                tokens[1].Type == "id" && 
+                tokens[1].Value == "a" &&
+                tokens[2].Type == "rparen" && 
+                tokens[2].Value == ")"
+                );
+
+            //Parser parser = new Parser(
+            //    new Parser.Rule(Parser.Rule.Type.And, "expr",
+            //        new Parser.Rule(Parser.Rule.Type.Token, "lparen"),
+            //        new Parser.Rule(Parser.Rule.Type.Token, "id"),
+            //        new Parser.Rule(Parser.Rule.Type.Token, "rparen")),
+            //    lexer);
+
+            //Parser.Result result = parser.Parse("( a )");
+            //System.Diagnostics.Debug.Assert(result != null);
+        }
+    }
+}
--- trunk/terp/libterp/Lexer.cs
+++ trunk/terp/libterp/Lexer.cs
@@ -7,119 +7,143 @@ using System.ComponentModel;
 
 namespace libterp
 {
-  public class Lexer
-  {
-    public class Rule
+    public class Lexer
     {
-      public Regex Regex { get; private set; }
-
-      public string Name { get; private set; }
-
-      public bool Delimiter { get; private set; }
-
-      public Rule(string name, string regex)
-        : this(name, regex, false)
-      {
-      }
-
-      public Rule(string name, string regex, bool delimiter)
-      {
-        Name = name;
-        Regex = new Regex(@"^" + regex);
-        Delimiter = delimiter;
-      }
-    };
-
-    public class Token
-    {
-      public string Type { get; private set; }
-
-      public string Value { get; private set; }
-
-      public Token(string type, string value)
-      {
-        Type = type;
-        Value = value;
-      }
-
-      public override string ToString()
-      {
-        return string.Format("{0}({1})", Type, Value);
-      }
-    };
-
-    public List<Rule> Rules { get; private set; }
-
-    public Lexer(params Rule[] rules)
-    {
-      Rules = new List<Rule>(rules);
-    }
-
-    public List<Token> Lex(string s)
-    {
-      List<Token> tokens = new List<Token>();
-      for (int i = 0; i < s.Length; )
-      {
-        bool matched = false;
-        foreach (Rule rule in Rules)
+        public class Rule
         {
-          if (!matched)
-          {
-            Match match = rule.Regex.Match(s.Substring(i));
-            if (match.Success)
+            public Regex Regex { get; private set; }
+
+            public string Name { get; private set; }
+
+            public bool Delimiter { get; private set; }
+
+            public Rule(string name, string regex)
+                : this(name, regex, false)
             {
-              if (!rule.Delimiter)
-              {
-                tokens.Add(new Token(rule.Name, s.Substring(i, match.Length)));
-              }
-              i += match.Length;
-              matched = true;
             }
-          }
-        }
-        if (!matched)
+
+            public Rule(string name, string regex, bool delimiter)
+            {
+                Name = name;
+                Regex = new Regex(@"^" + regex);
+                Delimiter = delimiter;
+            }
+        };
+
+        public class Result
         {
-          throw new Exception("lex error at position " + i + " " + s.Substring(i));
+            public string Type { get; private set; }
+
+            public string Value { get; private set; }
+
+            public Result(string type, string value)
+            {
+                Type = type;
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0}({1})", Type, Value);
+            }
+        };
+
+        public List<Rule> Rules { get; private set; }
+
+        public Lexer(params Rule[] rules)
+        {
+            Rules = new List<Rule>(rules);
         }
-      }
-      return tokens;
+
+        private class Context
+        {
+            public List<Result> Results { get; set; }
+
+            public string Str { get; set; }
+
+            public int Position { get; set; }
+
+            public Context()
+            {
+                Results = new List<Result>();
+                Position = 0;
+            }
+
+            public List<Context> Expand(List<Rule> rules)
+            {
+                List<Context> contexts = new List<Context>();
+                foreach (Rule rule in rules)
+                {
+                    Match match = rule.Regex.Match(Str.Substring(Position));
+                    if (match.Success && match.Length > 0)
+                    {
+                        Context context = new Context()
+                        {
+                            Str = Str,
+                            Position = Position + match.Length,
+                            Results = new List<Result>(Results)
+                        };
+                        if (!rule.Delimiter)
+                        {
+                            context.Results.Add(new Result(rule.Name, Str.Substring(Position, match.Length)));
+                        }
+                        contexts.Add(context);
+                    }
+                }
+                return contexts;
+            }
+        };
+
+        public List<Result> Lex(string str)
+        {
+            Queue<Context> contexts = new Queue<Context>();
+            contexts.Enqueue(new Context() { Str = str });
+            while (contexts.Any())
+            {
+                Context context = contexts.Dequeue();
+                if (context.Position == str.Length)
+                {
+                    return context.Results;
+                }
+                else
+                {
+                    context.Expand(Rules).ForEach(c => contexts.Enqueue(c));
+                }
+            }
+            return null;
+        }
     }
-  }
 }
 /**
- * enddecl = ;;
- * tokenop = =;
- * ruleop = =>;
- * orop = |;
- * andop = +;
- * lparen = (;
- * rparen = );
- * oneormoreop = *;
- * delimop = (delim);
- * id = \w+;
- * ws = \W+ (delim);
+ * regex = ""\S+""
+ * newline = "\n"
+ * equals = "="
+ * pipe = "|"
+ * plus = "+"
+ * lparen = "("
+ * rparen = ")"
+ * star = "*"
+ * id = "[a-zA-Z]\w+"
+ * ws *= "\s+"
  * 
- * grammar => decl*
- * decl => ( rule | token ) enddecl;
- * decl2 => rule | token
- * token => id tokenop ( id | id delimop );
- * rule => id rulop ruledecl;
- * ruledecl => id | 
- *             ruledecl orop ruledecl | 
- *             ruledecl andop ruledecl | 
- *             lparen ruledecl rparen | 
- *             ruledecl oneormoreop;
- *
- * grammar => decl*
- * decl => decl2 enddecl;
- * decl2 => rule | token;
- * token => id tokenop token2;
- * token2 => ( id | delimtoken );
- * delimtoken => id delimop;
- * rule => id rulop ruledecl;
- * ruledecl => id | ruledeclor | ruledecland | ruledeclparen | ruledecloneormore;
- * ruledeclor => ruledecl orop ruledecl;
- * ruledecland => ruledecl andop ruledecl;
- * ruledeclparen => lparen ruledecl rparen;
- * ruledecloneormore => ruledecl oneormoreop;
+ * grammar = line*
+ * line = decl newline | newline
+ * decl = rule | token | delimToken
+ * token = id equals regex
+ * delimToken = id star equals regex
+ * rule = id equals ruleDecl
+ * ruleDecl = id
+ *             | ruleDeclStar
+ *             | ruleDeclPlus
+ *             | ruleDeclAnd
+ *             | ruleDeclOr
+ *             | ruleDeclParen
+ * ruleDeclOr = ruleDecl ruleDeclOr_repeat
+ * ruleDeclOr_repeat = ruleDecl_repeatContent+
+ * ruleDeclOr_repeatContent = pipe ruleDecl
+ * ruleDeclAnd = ruleDecl ruleDeclAnd_repeat
+ * ruleDeclAnd_repeat = ruleDecl+
+ * ruleDeclParen = lparen ruleDecl rparen
+ * ruleDeclPlus = ruleDecl plus
+ * ruleDeclStar = ruleDecl star
  **/
