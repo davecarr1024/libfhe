0ce90f91970a831d519714871b46686e5a4bdccf
dave.carr.re2@gmail.com <dave.carr.re2@gmail.com@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2012-09-26 20:51:22 +0000

    terpy python

---
+++ terp2/terp/Lexer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace terp
+{
+  public class Lexer
+  {
+    public class Rule
+    {
+      public string Name { get; set; }
+
+      public Regex Regex { get; set; }
+
+      public bool Delimiter { get; set; }
+
+      public int Match(string input, int position)
+      {
+        Match match = Regex.Match(input.Substring(position));
+        if (match.Success && match.Index == 0 && match.Length > 0)
+        {
+          return match.Length;
+        }
+        else
+        {
+          return 0;
+        }
+      }
+    }
+
+    public class Result
+    {
+      public string Type { get; set; }
+
+      public string Value { get; set; }
+    }
+
+    public List<Rule> Rules { get; set; }
+
+    public List<Result> Lex(string input)
+    {
+      List<Result> results = new List<Result>();
+      int position = 0;
+      while (position < input.Length)
+      {
+        bool found = false;
+        foreach (Rule rule in Rules)
+        {
+          if (!found)
+          {
+            int length = rule.Match(input, position);
+            if (length > 0)
+            {
+              found = true;
+              if (!rule.Delimiter)
+              {
+                results.Add(new Result() { Type = rule.Name, Value = input.Substring(position, length) });
+              }
+              position += length;
+            }
+          }
+        }
+        if (!found)
+        {
+          throw new Exception("lex error at " + position + ": " + input.Substring(position));
+        }
+      }
+      return results;
+    }
+
+    public Lexer()
+    {
+      Rules = new List<Rule>();
+    }
+  }
+}
---
+++ terp2/terptest/TerpTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using terp;
+
+namespace terptest
+{
+  /// <summary>
+  /// Summary description for TerpTest
+  /// </summary>
+  [TestClass]
+  public class TerpTest
+  {
+    public TerpTest()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    [TestMethod]
+    public void assigment()
+    {
+      Scope scope = VirtualMachine.DefaultScope();
+      new Assignment() { Name = "a", Value = new Literal() { Value = new IntValue() { Value = 12 } } }.Evaluate(scope);
+      IntValue a = scope.GetValue("a") as IntValue;
+      Assert.IsNotNull(a);
+      Assert.AreEqual(12, a.Value);
+    }
+
+    [TestMethod]
+    public void functionReturn()
+    {
+      Scope scope = VirtualMachine.DefaultScope();
+      StringValue ret = new Function() { Expressions = { new Literal() { Value = new StringValue() { Value = "herro" } } } }.Apply(scope, new List<Value>()) as StringValue;
+      Assert.IsNotNull(ret);
+      Assert.AreEqual("herro", ret.Value);
+    }
+
+    [TestMethod]
+    public void functionArgs()
+    {
+      Scope scope = VirtualMachine.DefaultScope();
+      IntValue ret = new Function()
+      {
+        Parameters = { new Parameter() { Name = "foo" } },
+        Expressions = { new Variable() { Name = "foo" } }
+      }.Apply(scope, new List<Value>() { new IntValue() { Value = -1 } }) as IntValue;
+      Assert.IsNotNull(ret);
+      Assert.AreEqual(-1, ret.Value);
+    }
+
+    [TestMethod]
+    public void ctor()
+    {
+      Scope scope = VirtualMachine.DefaultScope();
+      scope.SetValue("foo", new Class() { Name = "foo", });
+      scope.SetValue("foo.__init__",
+        new Function()
+        {
+          Parameters = { new Parameter() { Name = "this" } },
+          Expressions = { new Assignment() { Name = "this.a", Value = new Literal() { Value = new IntValue() { Value = 11 } } } }
+        }
+      );
+      new Assignment(){Name = "bar", Value = new Invocation() { Functor = new Variable() { Name = "foo" } }}.Evaluate(scope);
+      IntValue a = scope.GetValue("bar.a") as IntValue;
+      Assert.IsNotNull(a);
+      Assert.AreEqual(11, a.Value);
+      Assert.IsNull(scope.GetValue("foo.a"));
+    }
+  }
+}
