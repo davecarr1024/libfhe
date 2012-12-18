8667b12d9aca9cc3db6d94f801bb02bb8308b2d0
dave.carr.re2@gmail.com <dave.carr.re2@gmail.com@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2012-12-18 17:50:34 +0000

    trimesh

--- trunk/sabo/World.py
+++ trunk/sabo/World.py
@@ -1,8 +1,12 @@
+from Vec2 import Vec2
 import time
 
 class World:
   def __init__( self, bodies = [] ):
     self.bodies = bodies
+    self.collisionPropGain = 10
+    self.collisionDiffGain = 1
+    self.gravity = Vec2( 0, -10 )
     
   def run( self, minDt = 0.01 ):
     last = time.time()
@@ -17,8 +21,12 @@ class World:
           if i != j:
             for pm in self.bodies[i].pointMasses:
               for shape in self.bodies[j].shapes:
-                if shape.isPointInside( pm.pos ):
-                  
+                diff = shape.test( pm.pos )
+                if diff != None:
+                  propForce = diff * self.collisionPropGain 
+                  diffForce = pm.vel.project( diff ) * -self.collisionDiffGain
+                  pm.applyForce( propForce + diffForce )
+              pm.applyForce( self.gravity * pm.mass )
       
       for body in self.bodies:
         body.update( dt )
