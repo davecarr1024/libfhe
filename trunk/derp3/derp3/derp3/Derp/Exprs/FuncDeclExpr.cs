52b38670a4bfb4685b49a4fd601bf31f82bacb13
dave.carr.re2@gmail.com <dave.carr.re2@gmail.com@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2014-04-07 21:01:05 +0000

    object oriented derp

---
+++ trunk/derp3/derp3/derp3/Derp/Exprs/FuncDeclExpr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace derp3
+{
+    public class FuncDeclExpr : Expr
+    {
+        public string Name { get; set; }
+
+        public List<string> Params { get; set; }
+
+        public List<Expr> Body { get; set; }
+
+        public FuncDeclExpr(string name, List<string> paramList, List<Expr> body)
+        {
+            Name = name;
+            Params = paramList;
+            Body = body;
+        }
+
+        public Val Eval(Scope scope)
+        {
+            return scope.Vals[Name] = new FuncVal(Params, Body);
+        }
+    }
+}
