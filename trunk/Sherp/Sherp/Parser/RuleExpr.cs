ad8777b26c148ca756be695c05e157fc5899a3af
dave.carr.re2@gmail.com <dave.carr.re2@gmail.com@a83f7b76-a30c-11de-8ba5-91705c1537ed>
2014-04-14 04:45:14 +0000

    nested parser

---
+++ trunk/Sherp/Sherp/Parser/RuleExpr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sherp.Parser
+{
+    public interface RuleExpr
+    {
+        string Name { get; set; }
+
+        Rule Bind(Dictionary<string, Rule> boundRules, Dictionary<string, RuleExpr> unboundRules, Lexer.Lexer lexer);
+    }
+}
