31682b423fc2255a10235f28c17cbdc7c6d7668b
David Carr <davecarr@google.com>
2016-08-22 16:11:06 -0400

    add chomp

---
+++ chomp/VirtualMachine.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Chomp
+{
+	public class VirtualMachine
+	{
+		public VirtualMachine ()
+		{
+		}
+	}
+}
+
---
+++ chomp/VirtualMachineTest.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System;
+
+namespace Chomp
+{
+	[TestFixture]
+	public class NUnitTestClass
+	{
+		[Test]
+		public void TestCase ()
+		{
+			Assert.IsTrue (false);
+		}
+	}
+}
+
